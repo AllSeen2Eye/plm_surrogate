ce66f2e824b9257825310db2d36e1c699eee1d00
Eye <27674655+AllSeen2Eye@users.noreply.github.com>
2026-01-22 21:10:12 +0000

    Update commons.py

--- commons.py
+++ commons.py
@@ -1,5 +1,26 @@
 import os
+import torch
 import numpy as np
+
+try:
+    import torch_xla as xla
+    import torch_xla.core.xla_model as xm
+    import torch_xla.distributed.parallel_loader as pl
+    import torch_xla.distributed.xla_multiprocessing as xmp
+    xla_device_available = True
+except ModuleNotFoundError:
+    xla_device_available = False
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+device = torch.device(device)
+print(f"Using {device} device")
+scaler = torch.amp.GradScaler(device)
 
 LOG_DIR, OUTPUT_DIR, CKPT_DIR, TRC_DIR, TMP_DIR = "./logs", "./models", "./checkpoints", "./trace", "./tmp"
 folders_to_create = [LOG_DIR, OUTPUT_DIR, CKPT_DIR, TRC_DIR, TMP_DIR]
