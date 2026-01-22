da93ba1dff05a82434dbe77911a35f54a0715787
Eye <27674655+AllSeen2Eye@users.noreply.github.com>
2026-01-22 21:08:08 +0000

    Update commons.py

--- commons.py
+++ commons.py
@@ -1,4 +1,11 @@
+import os
 import numpy as np
+
+LOG_DIR, OUTPUT_DIR, CKPT_DIR, TRC_DIR, TMP_DIR = "./logs", "./models", "./checkpoints", "./trace", "./tmp"
+folders_to_create = [LOG_DIR, OUTPUT_DIR, CKPT_DIR, TRC_DIR, TMP_DIR]
+for select_DIR in folders_to_create:
+    if not os.path.exists(select_DIR):
+        os.mkdir(select_DIR)
 
 aa_alphabet = "ACDEFGHIKLMNPQRSTVWYX"
 x_tokens = np.reshape(np.array(list(aa_alphabet)), (1, -1))
