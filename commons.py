1ae8b445a75e474fd5ed931662c2d70d81ef78b3
Eye <27674655+AllSeen2Eye@users.noreply.github.com>
2026-01-23 14:28:31 +0000

    Update commons.py
    Move xla check back...

--- commons.py
+++ commons.py
@@ -4,15 +4,6 @@ import requests
 import torch
 import numpy as np
 
-try:
-    import torch_xla as xla
-    import torch_xla.core.xla_model as xm
-    import torch_xla.distributed.parallel_loader as pl
-    import torch_xla.distributed.xla_multiprocessing as xmp
-    xla_device_available = True
-except ModuleNotFoundError:
-    xla_device_available = False
-
 def init_folders(folders_to_create):
     for select_DIR in folders_to_create:
         if not os.path.exists(select_DIR):
