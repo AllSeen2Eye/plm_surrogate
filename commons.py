c3edd2ccc44b039481d1296af62d0aa948657c3f
Eye <27674655+AllSeen2Eye@users.noreply.github.com>
2026-01-23 13:55:12 +0000

    Update commons.py
    Simplified - no return device fn, dynamic device estimation or just import of that data from outside

--- commons.py
+++ commons.py
@@ -1,33 +1,45 @@
 import os
+import requests 
+
 import torch
 import numpy as np
 
-def return_device_objects():
-    try:
-        import torch_xla as xla
-        import torch_xla.core.xla_model as xm
-        import torch_xla.distributed.parallel_loader as pl
-        import torch_xla.distributed.xla_multiprocessing as xmp
-        xla_device_available = True
-    except ModuleNotFoundError:
-        xla_device_available = False
-    
-    device = (
-        "cuda"
-        if torch.cuda.is_available()
-        else "mps"
-        if torch.backends.mps.is_available()
-        else "cpu"
-    )
-    device = torch.device(device)
-    print(f"Using {device} device")
-    scaler = torch.amp.GradScaler(device)
-    return device, xla_device_available, scaler
+try:
+    import torch_xla as xla
+    import torch_xla.core.xla_model as xm
+    import torch_xla.distributed.parallel_loader as pl
+    import torch_xla.distributed.xla_multiprocessing as xmp
+    xla_device_available = True
+except ModuleNotFoundError:
+    xla_device_available = False
 
 def init_folders(folders_to_create):
     for select_DIR in folders_to_create:
         if not os.path.exists(select_DIR):
             os.mkdir(select_DIR)
+
+def get_secondary_from_pdb(pdb_id_list, tmp_dir):
+    dataset_dict = {}
+    for dict_item in tqdm(pdb_id_list):
+        pdb_code, chain_id = dict_item
+        pdb_data = requests.get(f"https://files.rcsb.org/download/{pdb_code}.pdb").text
+        with open(f"{tmp_dir}/tmp_file.pdb", "w") as pdb_file_pointer:
+            pdb_file_pointer.write(pdb_data)
+        try:
+            traj = md.load(f"{tmp_dir}/tmp_file.pdb")
+            slice_chain = np.argwhere([atom.residue.chain.chain_id == chain_id for atom in list(traj.topology.atoms)])[:, 0]
+            traj = traj.atom_slice(slice_chain)
+            traj = traj.atom_slice(traj.topology.select("protein"))
+            dssp_list = md.compute_dssp(traj, simplified=False)[0].tolist()
+            if "NA" in dssp_list:
+                dssp_list = list(filter(lambda x: x != "NA", dssp_list))
+            dssp_seq = "".join(dssp_list).replace(" ", "C")
+            aminoacid_seq = traj.topology.to_fasta()[0]
+            dataset_dict[f"{pdb_code}_{chain_id}"] = [aminoacid_seq, dssp_seq]
+        except (ValueError, IndexError) as e:
+            print(e)
+    dataset_df = pd.DataFrame(dataset_dict, index = ["seq", "label"]).T
+    return dataset_df
 
 aa_alphabet = "ACDEFGHIKLMNPQRSTVWYX"
 x_tokens = np.reshape(np.array(list(aa_alphabet)), (1, -1))
