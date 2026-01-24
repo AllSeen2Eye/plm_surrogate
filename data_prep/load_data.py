a6938990abf5822cd54aedf05ce4c415214785b7
Eye <27674655+AllSeen2Eye@users.noreply.github.com>
2026-01-24 21:20:20 +0000

    Update load_data.py

--- data_prep/load_data.py
+++ data_prep/load_data.py
@@ -13,24 +13,27 @@ def load_files_from_pdb(pdb_id_list, save_dir):
             pdb_file_pointer.write(pdb_data)
 
 def get_secondary_from_pdb(pdb_id_list, save_dir, use_chain_id = True):
-    dataset_dict, real_idx = {}, []
+    dataset_dict = {}
     for dict_item in pdb_id_list:
         pdb_code, chain_id = dict_item
+        
         try:
             traj = md.load(f"{save_dir}/{pdb_code}.pdb")
-            slice_chain = np.argwhere([atom.residue.chain.chain_id == chain_id[0] for atom in list(traj.topology.atoms)])[:, 0]
             if use_chain_id:
+                slice_chain = np.argwhere([atom.residue.chain.chain_id == chain_id[0] for atom in list(traj.topology.atoms)])[:, 0]
                 traj = traj.atom_slice(slice_chain)
             traj = traj.atom_slice(traj.topology.select("protein"))
+            
             dssp_list = md.compute_dssp(traj, simplified=False)[0].tolist()
             if "NA" in dssp_list:
                 dssp_list = list(filter(lambda x: x != "NA", dssp_list))
             dssp_seq = "".join(dssp_list).replace(" ", "C").strip()
+            
             aminoacid_seq = traj.topology.to_fasta()[0]
-            dataset_dict[f"{pdb_code}_{chain_id}"] = [aminoacid_seq, dssp_seq]
-            real_idx.append(pdb_code+chain_id)
+            dataset_dict[f"{pdb_code}{chain_id}"] = [aminoacid_seq, dssp_seq]
+            
         except (ValueError, IndexError) as e:
             print(e)
+            
     dataset_df = pd.DataFrame(dataset_dict, index = ["seq", "label"]).T
-    dataset_df.index = real_idx
     return dataset_df
