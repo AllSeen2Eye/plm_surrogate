9b404ff304808c94e97d4a03029e2534af4605c1
Eye <27674655+AllSeen2Eye@users.noreply.github.com>
2026-01-31 20:04:45 +0000

    Update architecture.py

--- model/architecture.py
+++ model/architecture.py
@@ -526,7 +526,10 @@ class AttnDecompClassCoherence(nn.Module):
     
     def forward(self, input_dict): 
         lp = unpack_parameter_list(self.parameter_list) #local_params 
-        all_x_all_ampl = lp.all_x_all_ampl if not self.decompose_axa else lp.all_x_all_base*(lp.all_x_all_weights+1)
+        if not self.decompose_axa
+            all_x_all_ampl = lp.all_x_all_ampl 
+        else:
+            all_x_all_ampl = lp.all_x_all_base + (lp.all_x_all_weights + 1)
         class_attn_rec_0, class_attn_rec_1 = all_x_all_ampl[0], all_x_all_ampl[1], 
         class_attn_rec_2, class_attn_rec_3 = all_x_all_ampl[2], all_x_all_ampl[3:]   
         b_attn_0, b_attn_1, b_attn_2 = lp.all_x_all_bias
@@ -619,7 +622,10 @@ class WiderAttnDecompClassCoherence(nn.Module):
     
     def forward(self, input_dict): 
         lp = unpack_parameter_list(self.parameter_list) #local_params 
-        all_x_all_ampl = lp.all_x_all_ampl_1 if not self.decompose_axa else lp.all_x_all_base_1*(lp.all_x_all_weights_1+1)
+        if not self.decompose_axa
+            all_x_all_ampl = lp.all_x_all_ampl_1
+        else:
+            all_x_all_ampl = lp.all_x_all_base_1 + (lp.all_x_all_weights_1 + 1)
         class_attn_rec_0, class_attn_rec_1 = all_x_all_ampl[0], all_x_all_ampl[1], 
         class_attn_rec_2, class_attn_rec_3 = all_x_all_ampl[2], all_x_all_ampl[3:]     
         b_attn_0, b_attn_1, b_attn_2, b_attn_3, class_multiplier = lp.all_x_all_bias_1
@@ -727,7 +733,10 @@ class EvenWiderAttnDecompClassCoherence(nn.Module):
     
     def forward(self, input_dict): 
         lp = unpack_parameter_list(self.parameter_list) #local_params 
-        all_x_all_ampl = lp.all_x_all_ampl_2 if not self.decompose_axa else lp.all_x_all_base_2*(lp.all_x_all_weights_2+1)
+        if not self.decompose_axa
+            all_x_all_ampl = lp.all_x_all_ampl_2
+        else:
+            all_x_all_ampl = lp.all_x_all_base_2 + (lp.all_x_all_weights_2 + 1)
         class_attn_rec_0, class_attn_rec_1 = all_x_all_ampl[0], all_x_all_ampl[1], 
         class_attn_rec_2, class_attn_rec_3 = all_x_all_ampl[2], all_x_all_ampl[3:]
         b_attn_0, b_attn_1, b_attn_2, b_attn_3, class_multiplier = lp.all_x_all_bias_2
